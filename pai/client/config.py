785a4f2867be37fb47ca4bb7ad3e227a15fdac1d
endaaman <buhibuhidog@gmail.com>
2020-05-29 22:04:21 +0900

    h264 support

--- pai/client/config.py
+++ pai/client/config.py
@@ -2,16 +2,20 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--test', action='store_true')
-parser.add_argument('-a', '--api-host', default='http://localhost:3000')
+parser.add_argument('--dummy', action='store_true')
+parser.add_argument('--h264', action='store_true')
+parser.add_argument('--api-host', default='http://localhost:3000')
 parser.add_argument('--device', default='/dev/video0')
 args = parser.parse_args()
 
-DEVICE=args.device
-if args.test:
-    GST_SOURCE = 'videotestsrc ! clockoverlay'
+DEVICE = args.device
+if args.dummy:
+    GST_SOURCE = ' ! '.join(['videotestsrc', 'clockoverlay'])
 else:
-    GST_SOURCE = f'v4l2src device={DEVICE} ! videoconvert'
+    ss = [f'v4l2src device={DEVICE}', 'videoconvert']
+    if args.h264:
+        ss.insert(1, 'avdec_h264')
+    GST_SOURCE = ' ! '.join(ss)
 
 API_HOST = args.api_host
 RECONNECT_INTERVAL = 5
